663814b286fa88c804e99c599cf72181c895ddc4
nando-castro <fernando.castro10.master@gmail.com>
2022-09-12 23:16:53 -0300

    feat: config initial

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import "express-async-errors";
+import router from "./routes/index";
+
+dotenv.config();
+const app = express();
+app.use(json());
+app.use(cors());
+app.use(router);
+
+const PORT: number = Number(process.env.PORT);
+
+app.listen(PORT, () => {
+  console.log(`server running on PORT ${PORT}`);
+});
